e18fa34e0501ec281e5d7775a6c5b06a02ba1f95
Ramphys Tirana Brito <ramphystiranabrito@gmail.com>
2025-12-25 23:49:31 -0400

    keep safe the environment variables

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,10 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-// IMPORTANTE: Reemplaza estos valores con los de tu proyecto Supabase
-const SUPABASE_URL = "https://izirwkulpwtdmuqwbzpl.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6aXJ3a3VscHd0ZG11cXdienBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1MDY1NjUsImV4cCI6MjA3NzA4MjU2NX0.DImAc2K1eFuE7fMs1nouuQnmIFdjMTYiEbRD5mQaPAs";
-// const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-// const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+  throw new Error(
+    "Missing Supabase environment variables. Please check your .env file."
+  );
+}
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
